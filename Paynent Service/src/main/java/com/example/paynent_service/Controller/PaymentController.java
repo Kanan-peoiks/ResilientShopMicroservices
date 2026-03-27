3422cf4f923e1fb8058479b365d2b3fa9eb9d6f8
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-03-27 20:52:44 +0400

    feat: add PaymentController

---
+++ Paynent Service/src/main/java/com/example/paynent_service/Controller/PaymentController.java
@@ -0,0 +1,18 @@
+package com.example.paynent_service.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@RestController
+public class PaymentController {
+
+    @GetMapping("/payment/process")
+    public String processPayment() {
+        if (ThreadLocalRandom.current().nextInt(10) < 4) {
+            throw new RuntimeException("Payment service random failure");
+        }
+        return "Payment processed";
+    }
+}
