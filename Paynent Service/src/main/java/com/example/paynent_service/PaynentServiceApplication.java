937620caa4e0b69fce7e447c34af44456e7101ba
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-03-27 20:36:33 +0400

    add Payment service

---
+++ Paynent Service/src/main/java/com/example/paynent_service/PaynentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.paynent_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaynentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaynentServiceApplication.class, args);
+    }
+
+}
---
+++ Paynent Service/src/test/java/com/example/paynent_service/PaynentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paynent_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaynentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
