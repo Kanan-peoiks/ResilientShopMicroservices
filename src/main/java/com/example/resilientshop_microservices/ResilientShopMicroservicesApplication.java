8a5bed12b89b3d425202c82cd2a1c13147975d4a
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-03-27 19:41:12 +0400

    Initial commit

---
+++ src/main/java/com/example/resilientshop_microservices/ResilientShopMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.resilientshop_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResilientShopMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResilientShopMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/resilientshop_microservices/ResilientShopMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resilientshop_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResilientShopMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
